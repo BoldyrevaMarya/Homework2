ac24b10c03061137360f7e40c6eff2e244894f22
BoldyrevaMarya <matazzz@mail.ru>
2024-06-29 19:30:16 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_homework_completed = 12
+number_of_hours_spent =1.5
+course_name = "Python"
+time_for_one_task =0.125
+print("Курс:",course_name,",всего задач:",number_of_homework_completed,",затрачено часов:",
+      number_of_hours_spent,",среднее время выполнения",time_for_one_task,"часа")
